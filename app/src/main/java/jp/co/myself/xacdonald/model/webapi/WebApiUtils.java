c6b110f8b4a3165ef2ff2bc64652e2253d4e6f68
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-02-03 22:24:35 +0900

    ・ホーム画面用のWebAPI呼び出しを監視可能なオブジェクト生成メソッドを追加する。

--- app/src/main/java/jp/co/myself/xacdonald/model/webapi/WebApiUtils.java
+++ app/src/main/java/jp/co/myself/xacdonald/model/webapi/WebApiUtils.java
@@ -12,14 +12,20 @@ public class WebApiUtils {
 
     private static final String YAHOO_API_APP_ID = "dj00aiZpPWVheHgxT3VmSmp0eSZzPWNvbnN1bWVyc2VjcmV0Jng9YzU-";
 
-    public static Observable<ItemSearchResult> getItemSearchForHome() {
+    private static YahooApiInterface createYahooApi() {
 
         Retrofit retro = new Retrofit.Builder()
                 .baseUrl(YAHOO_API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
-        YahooApiInterface yai = retro.create(YahooApiInterface.class);
+        return retro.create(YahooApiInterface.class);
+
+    }
+
+    public static Observable<ItemSearchResult> getLowPriceItemForHome() {
+
+        YahooApiInterface yai = createYahooApi();
         return yai.itemSearchForHome(
                 YAHOO_API_APP_ID,
                 "new",
@@ -27,6 +33,19 @@ public class WebApiUtils {
                 300,
                 10,
                 "+price");
+
+    }
+
+    public static Observable<ItemSearchResult> getHighScoreItemForHome() {
+
+        YahooApiInterface yai = createYahooApi();
+        return yai.itemSearchForHome(
+                YAHOO_API_APP_ID,
+                "new",
+                50521,
+                76,
+                4,
+                "-score");
 
     }
 
