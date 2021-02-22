59ac8b4cd496fd3cf6038116f863c735abd7273d
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-02-23 07:22:25 +0900

    ・ホーム画面用のWebAPIリクエストで取得する項目（商品名、価格）を追加する。

--- app/src/main/java/jp/co/myself/xacdonald/model/webapi/home/Hit.java
+++ app/src/main/java/jp/co/myself/xacdonald/model/webapi/home/Hit.java
@@ -4,6 +4,10 @@ public class Hit {
 
     private ExImage exImage;
 
+    private String name;
+
+    private String price;
+
     public Hit(ExImage exImage) {
         this.exImage = exImage;
     }
@@ -14,6 +18,22 @@ public class Hit {
 
     public void setExImage(ExImage exImage) {
         this.exImage = exImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
     }
 
 }
