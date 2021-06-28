af966a1808f53899ad856fbc13ea22ce39ad742c
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-06-28 19:07:37 +0900

    ・ナビゲーションによる遷移時に、フラグメントを削除するパターン（遷移元のフラグメントと遷移先のフラグメントの組み合わせ）を追加する。

--- app/src/main/java/jp/co/myself/xacdonald/utils/RemoveFragmentPairConstant.java
+++ app/src/main/java/jp/co/myself/xacdonald/utils/RemoveFragmentPairConstant.java
@@ -6,12 +6,14 @@ import java.util.List;
 import jp.co.myself.xacdonald.fragment.MenuDetailFragment;
 import jp.co.myself.xacdonald.fragment.MenuFragment;
 import jp.co.myself.xacdonald.fragment.MenuInfoDetailFragment;
+import jp.co.myself.xacdonald.fragment.ShopFragment;
 
 public interface RemoveFragmentPairConstant {
     List<RemoveFragmentPair> LIST = new ArrayList() {
         {
             add(new RemoveFragmentPair(MenuDetailFragment.class, MenuFragment.class));
             add(new RemoveFragmentPair(MenuInfoDetailFragment.class, MenuDetailFragment.class));
+            add(new RemoveFragmentPair(ShopFragment.class, MenuDetailFragment.class));
         }
     };
 }
