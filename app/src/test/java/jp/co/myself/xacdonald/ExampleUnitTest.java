affdfc06a2beeeb0a33bde4f800f4996f55cf703
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-11-04 05:39:47 +0900

    ・import文を最適化する。

--- app/src/test/java/jp/co/myself/xacdonald/ExampleUnitTest.java
+++ app/src/test/java/jp/co/myself/xacdonald/ExampleUnitTest.java
@@ -1,8 +1,8 @@
 package jp.co.myself.xacdonald;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
